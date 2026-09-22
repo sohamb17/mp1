9cbedf6557d444b511bc839d91adf576da65b7c7
sohamb17 <sohambelurgikar17@gmail.com>
2026-09-22 01:32:21 -0500

    Make video autoplay

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,14 @@ import { initNav } from './nav';
 import { initCarousel } from './carousel';
 import { initModal } from './modal';
 import { initReveal } from './reveal';
+import { initVideoAutoplay } from './video';
 
 function init() {
   initNav();
   initCarousel();
   initModal();
   initReveal();
+  initVideoAutoplay();
 }
 
 if (document.readyState === 'loading') {
---
+++ src/js/video.js
@@ -0,0 +1,19 @@
+export function initVideoAutoplay() {
+  const video = document.querySelector('.about__video-el');
+  if (!video || !('IntersectionObserver' in window)) return;
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          video.play().catch(() => {});
+        } else {
+          video.pause();
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+  observer.observe(video);
+}
